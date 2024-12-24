4289b0f6a8c02385517e6442f5aaca0fc0516f66
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2024-12-25 03:32:08 +0530

    add Contributors page component 🎉

---
+++ app/contributors/page.tsx
@@ -0,0 +1,11 @@
+export default function page() {
+  return (
+    <section className="space-y-6 pb-8 pt-6 md:pb-12 md:mt-10 lg:py-32">
+      <div className="container flex flex-col gap-4 text-center">
+        <h1 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-balance">
+          Top Contributors
+        </h1>
+      </div>
+    </section>
+  );
+}
