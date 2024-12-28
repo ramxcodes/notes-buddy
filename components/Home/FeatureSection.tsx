9476fac14bcbdbb02ee64bfa94e99def301f4664
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2024-12-28 23:38:37 +0530

    Add hero and feature section 🫡

---
+++ components/Home/FeatureSection.tsx
@@ -0,0 +1,126 @@
+import Image from "next/image";
+import React from "react";
+
+function FeatureSection() {
+  return (
+    <>
+      <h1 className="h1-style my-4">
+        Why Notes Buddy?
+      </h1>
+      <div className="container mx-auto px-10 py-8">
+        <section className="grid md:grid-cols-3 gap-6 max-md:max-w-xs mx-auto ">
+          <div className="group  bg-gradient-to-t from-[#242424] to-[#020202] hover:from-[#182135] hover:to-[#080808] relative before:absolute before:inset-0 before:bg-[url('/noise.gif')] before:opacity-5 rounded-2xl border ">
+            <div className="relative">
+              <div className="px-6 py-5">
+                <div className="group-hover:bg-blue-400 bg-white transition-all duration-500 ease-in-out w-fit px-3 rounded-full text-sm py-1 text-black group-hover:text-white mb-1">
+                  Concepts
+                </div>
+                <span className="text-lg group-hover:hidden inline-block font-semibold pt-2 text-slate-200 mb-1 transition-all duration-500 ease-in-out">
+                  Overwhelmed with Complexity
+                </span>
+                <span className="text-lg group-hover:inline-block hidden font-semibold pt-2 text-slate-200 mb-1 transition-all duration-500 ease-in-out">
+                  Clarity Over Complexity
+                </span>
+                <p className="text-sm text-slate-500">
+                  Notes Buddy simplifies concepts for deeper and better
+                  understanding.
+                </p>
+              </div>
+              <div className="flex items-center justify-center group-hover:-translate-y-2 transition-transform duration-500 ease-in-out">
+                <Image
+                  className="group-hover:opacity-0 transition-opacity duration-500 object-cover h-full m-0 p-0"
+                  src="/featured/card-1-b.png"
+                  width={350}
+                  height={240}
+                  alt="Card Before"
+                />
+                <Image
+                  className="absolute top-0 left-0 opacity-0 group-hover:opacity-100 transition-opacity object-cover duration-300 h-full m-0 p-0"
+                  src="/featured/card-1-a.png"
+                  width={350}
+                  height={240}
+                  alt="Card After"
+                  aria-hidden="true"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="group  bg-gradient-to-t from-[#050a0a] to-[#051818] hover:from-[#05070a] hover:to-[#0b1a3b] relative before:absolute before:inset-0 before:bg-[url('/noise.gif')] before:opacity-5 rounded-2xl border ">
+            <div className="relative">
+              <div className="px-6 py-5">
+                <div className="bg-green-400 group-hover:bg-blue-600  transition-all duration-500 ease-in-out w-fit px-3 rounded-full text-sm py-1 text-white mb-1">
+                  AI
+                </div>
+                <span className="text-lg group-hover:hidden inline-block font-semibold pt-2 text-slate-200 mb-1 transition-all duration-500 ease-in-out">
+                  Answers at Your Fingertips
+                </span>
+                <span className="text-lg group-hover:inline-block hidden font-semibold pt-2 text-slate-200 mb-1 transition-all duration-500 ease-in-out">
+                  Doubts Left Unanswered
+                </span>
+                <p className="text-sm text-slate-500">
+                  Our AI answers your questions instantly, helping you learn
+                  faster.
+                </p>
+              </div>
+              <div className="flex items-center justify-center group-hover:-translate-y-2 transition-transform duration-500 ease-in-out">
+                <Image
+                  className="group-hover:opacity-0 transition-opacity duration-500"
+                  src="/featured/card-2-a.png"
+                  width={350}
+                  height={240}
+                  alt="Card image 01"
+                />
+                <Image
+                  className="absolute top-0 left-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300"
+                  src="/featured/card-2-b.png"
+                  width={350}
+                  height={240}
+                  alt="Card image 01 displaying on hover"
+                  aria-hidden="true"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="group  bg-gradient-to-t from-[#171c35] to-[#000000] hover:from-[#2b131e] hover:to-[#141414] relative before:absolute before:inset-0 before:bg-[url('/noise.gif')] before:opacity-5  rounded-2xl border ">
+            <div className="relative">
+              <div className="px-6 py-5">
+                <div className="bg-blue-400 group-hover:bg-red-500 transition-all duration-500 ease-in-out w-fit px-3 rounded-full text-sm py-1 text-white mb-1">
+                  Speed
+                </div>
+                <span className="text-lg group-hover:hidden inline-block font-semibold pt-2 text-slate-200 mb-1 transition-all duration-500 ease-in-out">
+                  Fast and Efficient Learning
+                </span>
+                <span className="text-lg group-hover:inline-block hidden font-semibold pt-2 text-slate-200 mb-1 transition-all duration-500 ease-in-out">
+                  Slow and Time-Consuming
+                </span>
+                <p className="text-sm text-slate-500">
+                  Notes Buddy accelerates learning with quick, focused
+                  resources.
+                </p>
+              </div>
+              <div className="flex items-center justify-center group-hover:-translate-y-2 transition-transform duration-500 ease-in-out">
+                <Image
+                  className="group-hover:opacity-0 transition-opacity duration-500"
+                  src="/featured/card-3-a.png"
+                  width={350}
+                  height={240}
+                  alt="Card image 01"
+                />
+                <Image
+                  className="absolute top-0 left-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300"
+                  src="/featured/card-3-b.png"
+                  width={350}
+                  height={240}
+                  alt="Card image 01 displaying on hover"
+                  aria-hidden="true"
+                />
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+}
+
+export default FeatureSection;
---
+++ components/Home/HeroSection.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import { Spotlight } from "../ui/spotlight";
+import { HeroSlideTextEffect } from "./HeroSlideTextEffect";
+import Tooltip from "./Tooltip";
+
+export default function HeroSection() {
+  return (
+    <div className="h-[40rem] w-full rounded-md flex flex-col md:items-center md:justify-center dark:bg-[#010510]/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden items-start justify-center container">
+      <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="white"
+      />
+      <div className="text-center space-y-2 container mx-auto px-4 md:px-0">
+        <h1 className="h1-style">
+          Preparation Fast
+          <br />
+          Study Smart
+        </h1>
+        <div className="text-[1rem] lg:text-[1.5rem]">
+          Notes Buddy is your one stop solution for{" "}
+          <HeroSlideTextEffect interval={1.3} className="font-bold">
+            {["Notes", "Flashcards", "One-Shots"]}
+          </HeroSlideTextEffect>
+        </div>
+      </div>
+      <div className="container lg:px-72 pt-20 flex flex-col md:flex-row items-center justify-between w-full space-y-6">
+        <div className="w-1/2 lg:w-3/4 flex items-center justify-center">
+          <Link
+            href={`/notes`}
+            className="bg-purple-500 text-white px-4 py-2 rounded-full text-center hover:bg-purple-600 w-44 block text-lg font-medium"
+          >
+            Read Notes →
+          </Link>
+        </div>
+        <div className="w-1/2 flex flex-col md:flex-row items-center justify-center py-4">
+          <Tooltip />
+          <span className="text-center ml-2 py-4 text-xs text-gray-500 dark:text-gray-400">
+            Rated by 1000+ students from 100+ colleges in India
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
