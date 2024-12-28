54cf5fc1ed9792eb53daf5c142cfee7ca65dc250
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2024-12-29 02:31:10 +0530

    Created Post and Footer Section 🐧

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import AboutSection from "@/components/Home/AboutSection";
 import FeatureSection from "@/components/Home/FeatureSection";
+import FooterSection from "@/components/Home/FooterSection";
 import HeroSection from "@/components/Home/HeroSection";
+import PostSection from "@/components/Home/PostSection";
 import MarqueeSlant from "@/components/MarqueeSlant";
 import { Sparkles } from "@/components/Sparkles";
 
@@ -23,6 +25,8 @@ export default function Home() {
         <AboutSection />
         <FeatureSection />
         <MarqueeSlant />
+        <PostSection />
+        <FooterSection />
       </div>
     </>
   );
