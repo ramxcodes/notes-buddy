2f8ccee0dda9fdcb99a64101062d426531d7b232
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2024-12-29 03:10:54 +0530

    Remove Sparkles component & marquee update 🧤

--- app/page.tsx
+++ app/page.tsx
@@ -13,17 +13,6 @@ export default function Home() {
       <FollowCursor />
       <div className="w-screen overflow-hidden relative">
         <HeroSection />
-        <div className="relative -mt-32 h-80 w-screen overflow-hidden [mask-image:radial-gradient(50%_50%,white,transparent)] before:absolute before:inset-0 before:bg-[radial-gradient(circle_at_bottom_center,#6d388a,transparent_90%)] before:opacity-40 after:absolute after:-left-1/2 after:top-1/2 after:aspect-[1/0.7] after:w-[200%] after:rounded-[10%] after:border-t after:border-[#6d388a] after:bg-[#b060dbbe] dark:after:bg-[#4b275f]">
-          <Sparkles
-            density={800}
-            speed={1.2}
-            size={3}
-            direction="top"
-            opacitySpeed={2}
-            color="#440467"
-            className="absolute inset-x-0 bottom-0 h-full w-full "
-          />
-        </div>
         <AboutSection />
         <FeatureSection />
         <MarqueeSlant />
