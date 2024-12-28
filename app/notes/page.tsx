f15337a9e8b26aa65e80fd3fc6d461679a0bf300
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2024-12-28 12:36:17 +0530

    Enhance Notes page UI 🥧

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -63,7 +63,17 @@ function BlogContent() {
   };
 
   return (
-    <div>
+    <div className="container max-w-4xl py-6 lg:py-10">
+      <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
+        <div className="flex-1 space-y-4">
+          <h1 className="inline-block font-black text-4xl lg:text-5xl">
+            Welcome to Notes
+          </h1>
+          <p className="text-xl text-muted-foreground">
+            Your exams last moment notes are here!
+          </p>
+        </div>
+      </div>
       <Card className="my-10">
         <CardHeader>
           <CardTitle>Search By :</CardTitle>
