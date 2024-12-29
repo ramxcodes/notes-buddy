ad990dfc581d17d3e7da6d2876a2bc10032026a3
Kavyansh Yadav <kavyansh.yadav.dev@gmail.com>
2024-12-29 12:05:03 +0530

    nd

--- app/api/search/route.tsx
+++ app/api/search/route.tsx
@@ -2,21 +2,22 @@ import { globalPrefixTree, initPrefixTree, PrefixTree } from '@/lib/autoComplete
 import { getAllNotesVelite } from '@/lib/getNotesJson';
 import { NextRequest, NextResponse } from 'next/server';
 
+
 export async function POST(req:NextRequest,res:NextResponse) {
   const q = await req.json()
   const ser:string = q.query
-  const tree = new PrefixTree();
-  
+  const tree = new PrefixTree();  
   if(!globalPrefixTree){
     await initPrefixTree();
   }
   let result = globalPrefixTree!._search(ser.toLowerCase());
-  if(result.length===0){
-    result = globalPrefixTree?._collectNotesWithFuzzySearch(ser.toLowerCase(), globalPrefixTree.root);
-    if(result.length >20){
-      result.slice(0,10);
-    }
-  }
+  // if(result.length===0){
+  //   result = globalPrefixTree?._collectNotesWithFuzzySearch(ser.toLowerCase(), globalPrefixTree.root);
+  //   if(result.length >20){
+  //     console.log("slicing")
+  //     result.slice(0,10);
+  //   }
+  // }
   // console.log(result)
   return NextResponse.json(result);
 }
