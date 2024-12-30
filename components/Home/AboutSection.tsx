a555199cf95984b7a67084bc3c34e5b99e03c95c
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2024-12-30 23:55:01 +0530

    refactor: add BlurFade component to enhance animation effects in AboutSection

--- components/Home/AboutSection.tsx
+++ components/Home/AboutSection.tsx
@@ -1,24 +1,29 @@
 import React from "react";
 import ScrollBaseAnimation from "@/components/text-marquee";
+import BlurFade from "../ui/blur-fade";
 
 function AboutSection() {
   return (
     <>
       <div className="h-[80px] md:h-[150px] lg:h-[500px] grid place-content-center w-fit">
-        <ScrollBaseAnimation
-          delay={500}
-          baseVelocity={-2}
-          classname="font-bold  leading-[110%] select-none"
-        >
-          Smarter Notes 🐧, Quick Answers 🏃🏻‍♀️, Flashcards 🗃️, PYQs 🤔
-        </ScrollBaseAnimation>
-        <ScrollBaseAnimation
-          delay={500}
-          baseVelocity={2}
-          classname="font-bold leading-[110%]  select-none"
-        >
-          Study Better with Notes Buddy 😎
-        </ScrollBaseAnimation>
+        <BlurFade delay={0.7}>
+          <ScrollBaseAnimation
+            delay={500}
+            baseVelocity={-2}
+            classname="font-bold  leading-[110%] select-none"
+          >
+            Smarter Notes 🐧, Quick Answers 🏃🏻‍♀️, Flashcards 🗃️, PYQs 🤔
+          </ScrollBaseAnimation>
+        </BlurFade>
+        <BlurFade delay={0.2} inView>
+          <ScrollBaseAnimation
+            delay={500}
+            baseVelocity={2}
+            classname="font-bold leading-[110%]  select-none"
+          >
+            Study Better with Notes Buddy 😎
+          </ScrollBaseAnimation>
+        </BlurFade>
       </div>
     </>
   );
